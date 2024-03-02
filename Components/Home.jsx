5887e0a343fdd0b3a9c3efc418b387c1d0cfefd2
Rose <i.a.rose.bk@gmail.com>
2024-03-02 10:37:39 -0500

    first commit

---
+++ Components/Home.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap'
+
+const Home = ({  }) => {
+    return (
+        <Container className="Welcome message">
+            <h1>
+                Welcome 
+            </h1>
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../Components/Home";
+function App() {
+  // const [count, setCount] = useState(0)
+  const apiKey = import.meta.env.VITE_API_KEY
+
+  return (
+    <>
+      <Home />
+      <Navbar />
+    </>
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+
+    //   </div>
+
+    // </>
+  );
+}
+
+export default App;
